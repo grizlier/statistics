1b0ed1c87f4cae8bb92d3ac958d3fd83ec833a97
grizlier <86568703+grizlier@users.noreply.github.com>
2026-02-07 17:48:25 +0500

    Initial

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+)
+
+type Student {
+	id 		int 		`json:"id"`
+	name 	string	`json:"name"`
+	entry int 		`json:"entry"`
+	time 	int 		`json:"time"`
+}
+
+var students []Student
+
+func main {
+	Load()
+
+	
+}
